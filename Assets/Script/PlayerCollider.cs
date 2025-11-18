b4689232d5f33302a6dd196443845687bcf954b0
seancho6616 <129930818+seancho6616@users.noreply.github.com>
2025-11-18 16:54:09 +0900

    플레이어
    아이템 충돌시 버튼 변경(이미지는 추후 수정)

--- Assets/Script/PlayerCollider.cs
+++ Assets/Script/PlayerCollider.cs
@@ -1,42 +1,50 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 
 // 캐릭터 충돌 관련 코드
 public class PlayerCollider : MonoBehaviour 
 {
     PlayerStemina playerStemina; 
     HeartManager heartManager;
+    AttackImageChanger attackImageChanger;
     void Start()
     {
         playerStemina = GetComponent<PlayerStemina>();
         heartManager = GetComponent<HeartManager>();
+        attackImageChanger = FindAnyObjectByType<AttackImageChanger>();
     }
     private void OnTriggerEnter(Collider other) {
+        
         if(other.gameObject.tag == "Stamina")
         {
-            Debug.Log("Hi Stamina");
-            if(playerStemina.MaxStamina > playerStemina.currentStamina)
-            {
-                playerStemina.currentStamina += 25f;
-                playerStemina.MaxStamina = 25f;    
-                playerStemina.UpdateStamina();
-            }
-            Destroy(other.gameObject);
+            attackImageChanger.ChangeSprite(); // 이미지 변경
+            // if(playerStemina.MaxStamina > playerStemina.currentStamina)
+            // {
+            //     playerStemina.currentStamina += 25f;
+            //     playerStemina.MaxStamina = 25f;    
+            //     playerStemina.UpdateStamina();
+            // }
+            // Destroy(other.gameObject);
         }
         if(other.gameObject.tag == "Heart")
         {
+            attackImageChanger.ChangeSprite();
             Debug.Log("Heart");
-            heartManager.MaxHealth =4f;
-            heartManager.MakeSameHeart();
-            Destroy(other.gameObject);
+            // heartManager.MaxHealth =4f;
+            // heartManager.MakeSameHeart();
+            // Destroy(other.gameObject);
+        }
+        if(other.gameObject.tag == "Potion")
+        {
+            attackImageChanger.ChangeSprite();
         }
         if(other.gameObject.tag == "Coin")
         {
             
         }
-        if(other.gameObject.tag == "Potion")
-        {
-            
-        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        attackImageChanger.BeforeChangeSprite();
     }
 }
